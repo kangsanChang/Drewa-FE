5e3d57832707aca467e2d45ea68255271d17a090
kangsan <rkdtks1005@gmail.com>
2018-01-09 16:14:23 +0900

    Add alternate fonts

--- applicant-fe/src/App.vue
+++ applicant-fe/src/App.vue
@@ -14,8 +14,7 @@
   /* 기본 margin (8px) 제거하기 위함 */
   body {
     margin: 0;
-    /* font-family: 'Noto Sans KR', sans-serif; */
-    font-family: 'NanumSquare', sams-serif;
+    font-family: 'Nanum Square', 'NanumSquare', 'Apple SD Gothic Neo', 'Nanum Gothic', sans-serif;
     background-color: rgb(235, 237, 239);
   }
 </style>
